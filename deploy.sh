d405cf145c3f4b8e52381cc904cdb35f76ad875c
milinddethe15 <milinddethe15@gmail.com>
2025-02-08 00:42:24 +0530

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+# Check if K8S_NAMESPACE is set, otherwise use "default"
+K8S_NAMESPACE=${K8S_NAMESPACE:-default}
+
 # Setup Kubeconfig
 echo $KUBE_CONFIG | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 # Apply Kubernetes Manifests
-kubectl apply -f k8s/
+kubectl apply -f k8s_manifest/
 
 # Restart deployments to use new images
 kubectl rollout restart deployment/chat-backend -n $K8S_NAMESPACE
